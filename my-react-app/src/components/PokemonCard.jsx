769ebf4b3bb6724a498bf595d32ee5ed850a0127
Helene Rampelberg <h.rampelberg@yahoo.fr>
2023-10-10 12:06:06 +0200

    add a function to display a pic into an array

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,13 +1,29 @@
-function PokemonCard(){
-  return( 
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",  
+    },
+    {
+        name: "Mew",
+    },
+];
+
+function PokemonCard() {
+    const pokemon = pokemonList[0];
+    return (
     <>
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+    {pokemon.imgSrc?(
+        <figure>
+     <img src= {pokemon.imgSrc} alt="bulbasaur" />
+     <figcaption>{pokemon.name}!!!</figcaption>
+     </figure>)
+    : (<p>???</p>)
+    }
     </>
-  );
-}
+)};
+  
+
+
 
 export default PokemonCard
